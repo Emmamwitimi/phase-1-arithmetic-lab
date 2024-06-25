10ef7ad533b322563bcbddec771e63012dc3e980
Emmaculate <emmamwitimi14@gmail.com>
2024-06-25 08:47:31 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+var num1 = 2 ;
+var num2 = 31 ;
+let multiply = num1 * num2 ;
+let random = Math.floor(Math.random() * 100) + 1;
+
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4; 
+
+let numbers = [5, 10, 20, 3, 15];
+let max = Math.max(...numbers); 
